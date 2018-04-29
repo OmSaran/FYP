22d2d20ad7e280b5bcc3278bdcacc9ec4ac69668
om <omiyorulz@gmail.com>
2018-04-29 20:21:36 +0530

    fix

--- Compiler/Machines/CompilerUtils/machineGenerator.js
+++ Compiler/Machines/CompilerUtils/machineGenerator.js
@@ -218,7 +218,7 @@ function createBot(syntaxTree, user, botName, cb)
 function updateBot(syntaxTree, user, botName, cb)
 {
     console.log('machineGenerator.js')
-    console.log(syntaxTree);
+    console.log(JSON.stringify(syntaxTree.intents.hi.response))
     var botCount = botName.split('_');
     botCount = botCount[botCount.length-1];
     let rootDialog = beautify(getDialog(syntaxTree, botCount));
